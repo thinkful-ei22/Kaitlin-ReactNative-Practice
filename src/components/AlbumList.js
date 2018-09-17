7a269c20ee03a504e27a119305a2d5b1dff1461c
Kaitlin Bunn <kabunn1991@gmail.com>
2018-09-17 19:50:32 -0400

    refactored component into a class component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 // const AlbumList = () => {
 class AlbumList extends Component {
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+  }
   // all classes need a render
   render() {
     return (
